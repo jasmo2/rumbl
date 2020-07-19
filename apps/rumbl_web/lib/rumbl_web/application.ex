3d7d0be185872fed25c03f3db162d2415f9d63f9
jaime <jaime.molina@mattersupply.co>
2020-07-19 14:56:45 -0500

    web resourcing files

--- apps/rumbl_web/lib/rumbl_web/application.ex
+++ apps/rumbl_web/lib/rumbl_web/application.ex
@@ -7,12 +7,8 @@ defmodule RumblWeb.Application do
 
   def start(_type, _args) do
     children = [
-      # Start the Telemetry supervisor
-      RumblWeb.Telemetry,
-      # Start the Endpoint (http/https)
-      RumblWeb.Endpoint
-      # Start a worker by calling: RumblWeb.Worker.start_link(arg)
-      # {RumblWeb.Worker, arg}
+      RumblWeb.Endpoint,
+      RumblWeb.Presence
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
